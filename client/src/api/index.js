b7c1ecc13f8066351148c01f54be1130f9c4af19
Ben Rosenberger <brose32@live.unc.edu>
2023-01-07 17:52:10 -0500

    add support for var port and url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+// const url = 'http://localhost:5000/posts';
+const url = '/posts'
 
 export const fetchPosts = () => axios.get(url);
 export const createArtist = (newPost) => axios.post(url, newPost);
